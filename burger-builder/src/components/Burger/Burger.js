9c95695281c9c28d908cdbb7784fd5ae6dca6edc
Duvanchi <cochize0@gmail.com>
2019-05-06 20:27:37 -0400

    Completed dynamic Modal, price calculation, and styling for current components

--- burger-builder/src/components/Burger/Burger.js
+++ burger-builder/src/components/Burger/Burger.js
@@ -14,7 +14,9 @@ const burger = (props) => {
     if (transformedIngredients.length === 0) {
         transformedIngredients = <p>Please Start Adding Ingredients</p>
     }
+    
     console.log(transformedIngredients)
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
